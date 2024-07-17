ce886b5d7a50a4fb829d06b1bb9e6a608212834b
TheBigNerd <ethanpotter0123@gmail.com>
2024-07-17 15:25:34 +0100

    Fixed admin product area - rewrote all product actions

--- app/admin/actions/products.ts
+++ app/admin/actions/products.ts
@@ -1,66 +1,90 @@
 "use server";
-import { notFound, redirect } from "next/navigation";
-import { prisma } from "@/lib/prisma";
 import { z } from "zod";
 import fs from "fs/promises";
+import { prisma } from "@/lib/prisma";
+import { notFound, redirect } from "next/navigation";
 
-const fileSchema = z.instanceof(File, { message: "Required" });
-const imageSchema = fileSchema.refine((file) => file.size > 0 && file.type.startsWith("image/"), { message: "Invalid image" });
+const fileSchema = z.instanceof(File, {message : "Required"})
+const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
 
 const addSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-  description: z.string().min(1, { message: "Description is required" }),
-  priceinPence: z.preprocess(
-    (val) => (val !== null && val !== undefined ? parseInt(val as string) : undefined),
-    z.number().int().min(1, { message: "Price must be a positive integer" })
-  ),
-  image: imageSchema,
-});
+  name: z.string().min(1),
+  description: z.string().min(1),
+  priceInPence: z.coerce.number().int().min(1),
+  image: imageSchema.refine(file => file.size > 0, "Required")
+})
 
-export async function AddProduct(prevState: unknown, formData: FormData) {
-  const formEntries = Object.fromEntries(formData.entries());
-  console.log("Form Data:", formEntries);
-
-
-  const imageFile = formData.get("image") as File;
-  if (!imageFile) {
-    console.error("Image file is missing");
-    return { image: ["Image file is required"] };
-  }
-
-
-  const result = addSchema.safeParse({ ...formEntries, image: imageFile });
+export async function addProduct(prevState: unknown, formData: FormData){
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
   if (result.success === false) {
-    console.error("Validation Errors:", result.error.formErrors.fieldErrors);
-    return result.error.formErrors.fieldErrors;
+    return result.error.formErrors.fieldErrors
   }
-  const data = result.data;
 
-  await fs.mkdir("public/products", { recursive: true });
-  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`;
-  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()));
+  const data = result.data
 
-  await prisma.product.create({
-    data: {
-      name: data.name,
-      description: data.description,
-      priceInPence: data.priceinPence,
-      imagePath,
-      isAvailable: false
-    },
-  });
+  await fs.mkdir("public/products", {recursive: true })
+  const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+  await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
 
-  redirect("/admin/products");
-}
+  await prisma.product.create({ data: {
+    isAvailable: false,
+    name: data.name,
+    description: data.description,
+    priceInPence: data.priceInPence,
+    imagePath
+  }})
 
-export async function toggleProductAvailability(id : string, isAvailable: boolean) {
-    await prisma.product.update({ where: { id }, data : { isAvailable }})
+  redirect("/admin/products")
 
 }
 
-export async function deleteProduct(id : string){
-    const product = await prisma.product.delete({ where: { id }})
-    if (product == null) return notFound()
+export async function toggleProductAvailability(
+  id: string,
+  isAvailable: boolean
+) {
+  await prisma.product.update({ where: { id }, data: {
+    isAvailable }})
+}
 
+export async function deleteProduct(id: string){
+  const product = await prisma.product.delete({ where: { id }})
+  if (product == null) return notFound()
+
+  fs.unlink(`public${product.imagePath}`)
+}
+
+const editSchema = addSchema.extend({
+  file:fileSchema.optional(),
+  image: imageSchema.optional()
+})
+
+export async function updateProduct( id: string, prevState: unknown, formData: FormData){
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()))
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors
+  }
+
+  const data = result.data
+  const product = await prisma.product.findUnique({ where: { id } })
+
+  if (product == null) return notFound()
+
+  let imagePath = product.imagePath
+  if (data.image != null && data.image.size > 0){
     await fs.unlink(`public${product.imagePath}`)
+    imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+  }
+
+
+  await prisma.product.update({ where: {id}, data: {
+    isAvailable: false,
+    name: data.name,
+    description: data.description,
+    priceInPence: data.priceInPence,
+    imagePath
+  }})
+
+  redirect("/admin/products")
+
 }
--- app/admin/products/productComponents/ProductForm.tsx
+++ app/admin/products/productComponents/ProductForm.tsx
@@ -6,56 +6,45 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
-import { AddProduct } from "../../actions/products";
+import { addProduct, updateProduct} from "../../actions/products";
 import { useFormState, useFormStatus } from "react-dom";
 import { Product } from "@prisma/client";
-import Link from "next/link"
-import Image from "next"
+import Image from "next/image";
 
-export function ProductForm( { product }: { product?: Product | null} ) {
-    const [error, action] = useFormState(AddProduct, {});
-    const [priceinPence, setPriceinPence] = useState<number | undefined>(product?.priceInPence);
+export function ProductForm({ product }: {product?: Product | null}) {
+  const [error, action] = useFormState(product == null ? addProduct: updateProduct.bind(null, product.id), {})
+  const [priceInPence, setPriceInPence] = useState<number | undefined>(product?.priceInPence)
 
-    return (
-        <form action={action} className="space-y-8">
-            <div className="space-y-2">
-                <Label htmlFor="name">Name</Label>
-                <Input type="text" id="name" name="name" required defaultValue={product?.name || "" } />
-                {error.name && <div className="text-destructive">{error.name}</div>}
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="priceinPence">Price In Pence</Label>
-                <Input
-                    type="number"
-                    id="priceinPence"
-                    name="priceinPence"
-                    required
-                    value={priceinPence}
-                    onChange={e => setPriceinPence(Number(e.target.value) || undefined)}
-                />
-                <div className="text-muted-foreground">{formatCurrency((priceinPence || 0) / 100)}</div>
-                {error.priceinPence && <div className="text-destructive">{error.priceinPence}</div>}
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="description">Description</Label>
-                <Textarea id="description" name="description" required defaultValue={product?.description || "" } />
-                {error.description && <div className="text-destructive">{error.description}</div>}
-            </div>
-            <div className="space-y-2">
-                <Label htmlFor="image">Product Image</Label>
-                <Input type="file" id="image" name="image" required={product == null} />
-                {error.image && <div className="text-destructive">{error.image}</div>}
-            </div>
-            <SubmitButton />
-        </form>
-    );
+
+  return <form action={action} className="space-y-8">
+    <div className="space-y-2">
+      <Label htmlFor="name">Name</Label>
+      <Input type="text" id="name" name="name" required defaultValue={product?.name || "" } />
+      {error.name && <div className="text-destructive">{error.name}</div>}
+    </div>
+    <div className="space-y-2">
+      <Label htmlFor="priceInPence">Price In Pence</Label>
+      <Input type="number" id="priceInPence" name="priceInPence" required value={priceInPence} onChange={e => setPriceInPence(Number(e.target.value) || undefined)} />
+    </div>
+    <div className="text-muted-foreground">{formatCurrency((priceInPence || 0) / 100)}</div>
+    {error.priceInPence && <div className="text-destructive">{error.priceInPence}</div>}
+    <div className="space-y-2">
+      <Label htmlFor="description">Description</Label>
+      <Textarea id="description" name="description" required defaultValue={product?.description || "" } />
+      {error.description && <div className="text-destructive">{error.description}</div>}
+    </div>
+    <div className="space-y-2">
+      <Label htmlFor="image">Image #1</Label>
+      <Input type="file" id="image" name="image" required={product == null} />
+      {product != null && <Image src={product.imagePath} height="400" width="400" alt="product image"/> }
+      {error.image && <div className="text-destructive">{error.image}</div>}
+    </div>
+    <SubmitButton/>
+  </form>
+
 }
 
 function SubmitButton() {
-    const { pending } = useFormStatus();
-    return (
-        <Button type="submit" disabled={pending}>
-            {pending ? "Saving..." : "Save"}
-        </Button>
-    );
+  const { pending } = useFormStatus()
+  return <Button type="submit" disabled={pending}>{pending ? "Saving..." : "Save"}</Button>
 }
