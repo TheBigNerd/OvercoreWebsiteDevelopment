fda4024a99543065f95d1f9ce7a4282f5623bc53
archa-dev <homidica@gmail.com>
2024-07-28 15:08:22 +0100

    Server Change - Changed email send addresses and setup domain as a variable

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,13 +1,14 @@
 import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendTwoFactorTokenEmail = async (
     email: string,
     token: string,
 ) => {
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "authentication@overcore.co.uk",
         to: email,
         subject: "Two Factor Authentication Code",
         html: `<p>Your Two Factor Authentication Code: ${token} </p>`
@@ -19,19 +20,19 @@ export const sendVerificationEmail = async (
     email: string,
     token: string,
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-    await resend.emails.send({ from: "onboarding@resend.dev", to: email, subject: "Confirm your email", html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email.</p>`})
+    await resend.emails.send({ from: "authentication@overcore.co.uk", to: email, subject: "Confirm your email", html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email.</p>`})
 }
 
 export const sendPasswordResetEmail = async (
     email: string,
     token: string
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `${domain}/auth/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "reset-password@overcore.co.uk",
         to: email,
         subject: "Reset your password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`})
