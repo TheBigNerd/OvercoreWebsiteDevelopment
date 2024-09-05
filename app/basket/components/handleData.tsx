ddc1cdf37a557fcc4c7d1cb444797014620080cc
Archa-dev <homidica@gmail.com>
2024-09-05 14:54:42 +0100

    feat: Add handleData component for retrieving cookie IDs

---
+++ app/basket/components/handleData.tsx
@@ -0,0 +1,17 @@
+import { parseCookies } from 'nookies';
+
+function getCookieIds(): string[] | null {
+    const cookies = parseCookies();
+    const cookieValue = cookies['productBasket'];
+
+    if (cookieValue) {
+        // Parse the cookie value and extract the IDs
+        const ids = cookieValue.split(',');
+
+        // Return the array of IDs
+        return ids;
+    }
+
+    // Return null if the cookie is not found
+    return null;
+}
