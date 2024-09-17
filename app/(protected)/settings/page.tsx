03b139a287b45c50cb77d630c5ba489a7f52f207
farhan11703 <farhan.r@outlook.ie>
2024-09-17 13:19:14 +0100

    Updated profile page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -49,7 +49,7 @@ const SettingsPage = () => {
         })
     }
     return (
-        <Card className="w-[600px]">
+        <Card className="bg-slate-100 w-[600px]">
             <CardHeader>
                 <p className="text-2xl font-semibold text-center">
                     Account Settings
