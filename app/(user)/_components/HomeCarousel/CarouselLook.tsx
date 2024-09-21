a74c08b435047a4a4bc58b6b8135b7a7c98a8ac9
PearOrchards <pkedzia88@gmail.com>
2024-09-21 21:27:00 +0100

    fix(favourites): changed "server action" to route handler, fixed all the errors

--- app/(user)/_components/HomeCarousel/CarouselLook.tsx
+++ app/(user)/_components/HomeCarousel/CarouselLook.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 import type { Product } from "@prisma/client";
 import { formatCurrency } from "@/lib/formatters";
 
-export default async function CarouselLook({ product } : { product: Product }) {
+export default function CarouselLook({ product } : { product: Product }) {
 	return (
 		<div className="carousel-item bg-gray-300 rounded-3xl px-8 py-4">
-			<Image src={product.imagePath} alt="Product Image" width={300} height={300} className="rounded-lg aspect-square object-contain"/>
+			<Image src={product.imagePath[0]} alt="Product Image" width={300} height={300} className="rounded-lg aspect-square object-contain"/>
 			<h1 className="product-name font-bold text-2xl pt-4">{product.name}</h1>
 			<p className="product-price">{formatCurrency(product.priceInPence / 100)}</p>
 		</div>
