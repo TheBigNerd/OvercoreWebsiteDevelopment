fde9339d9368d68623bb25af6d08cc90e525ae96
Archa-dev <hadleyarchie@gmail.com>
2025-01-09 14:47:26 +0000

    Update Business page layout with additional image and text sections

--- app/(Business)/b2b/page.tsx
+++ app/(Business)/b2b/page.tsx
@@ -11,8 +11,13 @@ export default function Page() {
             </div>
             <div className="flex justify-between items-center">
                 <p className="w-1/2 text-center">text here please darlin</p>
-                <img src="../images/BrowseWorkstation.jpg" alt="" className="w-1/4 mr-[12%] mb-[5%]" />
+                <img src="../images/BrowseWorkstation.jpg" alt="" className="w-1/4 mr-[12%] mt-[5%]" />
+            </div>
+            <div className="flex justify-between items-center">
+                <img src="../images/BrowseWorkstation.jpg" alt="" className="w-1/4 ml-[12%] mt-[5%] mb-[5%]" />
+                <p className="w-1/2 text-center">ahfhfajfafdajkdslfdjsafsalkjfjals hdadasfldsafalfdhdsa fahfdafhjakhf af hjafdjda hjfda lhfdldsfljafljfdsaldfjlsajfajfja ljds jhahj hjda hjdsaalffd ljfjfdajd fjda </p>
             </div>
         </div>
+
     )
 }
